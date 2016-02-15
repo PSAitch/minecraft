2d2dee2b2291a99638e71171cd954f3771d3ae0b
Pete Helliwell <pete@thehelliwells.org.uk>
2016-02-15 10:46:03 +0000

    minecraft_server.py changed to convert the base class to a TCP server, and add a lot of new API functions. minecraft shell script corrected to invoke the python script when checking for status

--- minecraft_server.py
+++ minecraft_server.py
@@ -7,14 +7,28 @@ import sys
 import io
 import os
 
+import logging
+import sys
 import time
-# fied strings
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(name)s: %(message)s',
+                    )
+
+# String Constants
+LOGGER_STREAM="MinecraftServer"
 FREE_MEM_CMD="""free -t -m | grep Mem | awk '{print $4}'"""
+GET_STATUS_CMD="""sudo ps all | grep java | grep -v grep | tr -d '[[:space:]]'"""
+SERVER_ROOT="/srv/minecraft"
 INI_FILE='/etc/minecraft.conf'
+PROPS_FILE=SERVER_ROOT+'/server.properties'
 LOG_FILE='/var/log/minecraft.log'
+KILL_CMD='/stop'
 KILL_SIGNAL='/api/send?command=/stop'
 KILL_RESP_OK='OK'
 KILL_RESP_BAD='BD'
+GET_PROPS_CMD="sudo ls "+SERVER_ROOT+" | grep '.properties' | grep -v grep"
+
 # code to extract a switch from an argument list 
 def _getSwitch(sSwitch,sArray):
     try:
@@ -79,11 +93,28 @@ def _getIntParam(sParam,sArray,iDefault=-1):
         print "Unhandled Exception" , sys.exc_info()[0]
 
 def _getIniValue(sParam):
-	fIni=io.open(INI_FILE)
-	liIni=fIni.readlines(-1)
-	fIni.close()
-	vRet=_getStrParam(sParam,liIni,'')
-	return vRet.strip()
+    fIni=io.open(INI_FILE)
+    liIni=fIni.readlines(-1)
+    fIni.close()
+    vRet=_getStrParam(sParam,liIni,'')
+    return vRet.strip()
+
+def _getMC_Value(param_key):
+    fConfig=io.open(PROPS_FILE,'r')
+    liConfig=fConfig.readlines()
+    fConfig.close()
+    vRet=_getStrParam(param_key,liConfig,'')
+    return vRet.strip()
+
+def _setMC_Value(param_key,param_value):
+    # copy the original to .bak
+    #TODO
+    # open the properties file and put it in a list
+    fConfig=io.open(PROPS_FILE,'r')
+    liConfig=fConfig.readlines()
+    fConfig.close()
+    # TODO write the lines out to the new file
+    # correcting the new value
 
 class MinecraftTCPHandler(SocketServer.BaseRequestHandler):
     """
@@ -97,11 +128,9 @@ class MinecraftTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
+        self.logger = logging.getLogger(LOGGER_STREAM)
+        self.logger.debug("request from {}:".format(self.client_address[0])+self.data)
         try:
-#            print "Service the request"
-            
             p=self.data.rfind("?")+1
             sp=self.data.find(" ")+1
             if p>0: # Not a parametered request
@@ -110,32 +139,71 @@ class MinecraftTCPHandler(SocketServer.BaseRequestHandler):
                 params=self.data[p:end].split("&")
             else:
                 sPage=self.data[sp:p-1]
-            if sPage =="/api/setval" and p>0:
+            if sPage =="/api/set" and p>0:# set server property
                 param_key=_getStrParam("key",params)
                 param_value=_getStrParam("value",params)
-                Set_MC_Value(param_key,param_value)
-            elif sPage =="/api/getval" and p>0:
-                param_key=_getStrParam("key",params)
-                param_value=Get_MC_Value(param_key)
+                _setMC_Value(param_key,param_value)
                 self.request.sendall(param_value)
-            elif sPage =="/api/send" and p>0:
+                self.logger.debug("property " +param_key+" set to " + param_value)
+            elif sPage =="/api/get" and p>0: # get server property
+                param_key=_getStrParam("key",params)
+                param_value=_getMC_Value(param_key)
+                self.request.sendall(param_value)
+                self.logger.debug("property " +param_key+" requested")
+            elif sPage =="/api/save" and p>0:# save server.properties file
+                param_value=_getStrParam("name",params)
+                if param_value!='':
+                    if _saveMC_PropsFile(param_value)
+                        self.request.sendall(KILL_RESP_OK)
+                        self.logger.debug("file " +param_value+" saved")
+                    else:
+                        self.request.sendall(KILL_RESP_BAD)
+                        self.logger.debug("file " +param_value+" not saved")
+                else:
+                    self.request.sendall(KILL_RESP_BAD)
+                    self.logger.debug("No name parameter supplied")
+            elif sPage =="/api/load" and p>0: # load server.properties file
+                param_value=_getStrParam("name",params)
+                if param_value!="":
+                    _loadMC_PropsFile(param_value)
+                    self.request.sendall(KILL_RESP_OK)
+                    self.logger.debug("loaded " +param_key+" file")
+                else:
+                    self.request.sendall(KILL_RESP_BAD)
+                    self.logger.debug("Unable to load " +param_key+" file")
+            elif sPage =="/api/list" and p>0: # load server.properties list
+                liFiles=_getMC_PropsFiles()
+                self.request.sendall(liFiles)
+                self.logger.debug("properties file list sent")
+            elif sPage =="/api/restart" and p>0: # load server.properties list
+                try
+                    self.server.restart_server()
+                    self.request.sendall(KILL_RESP_OK)
+                    self.logger.debug("instance restarted")
+                except:
+                    self.request.sendall(KILL_RESP_BAD)
+                    self.logger.debug("Error during restart")
+            elif sPage =="/api/send" and p>0:# send command to the console
                 send_text=_getStrParam("command",params)
-                print send_text
                 self.server.MC_Process.MC_Input.writeline(send_text)
-                resp=self.server.MC_Process.MC_Output.readline()
-#                self.server.MC_Process.proc.stdin.writeline(send_text)
-#                resp=self.server.MC_Process.proc.stdout.readline()
-		if send_text=='/stop':
+#                resp=self.server.MC_Process.MC_Output.readline()
+                self.logger.debug("Command " +send_text+" sent")
+		if send_text==KILL_CMD:
 	                self.request.sendall(KILL_RESP_OK)
-		else:
-			self.request.sendall(resp)
-            elif sPage =="/api/get":
-                resp=self.server.MC_Process.proc.stdout.readline()
+		else:# perhaps reform a request
+			self.request.sendall(send_text)
+            elif sPage =="/api/console":
+                fLogfile=io.open(LOG_FILE,'r')
+                resp=fLogfile.readall()
                 self.request.sendall(resp)
+                self.logger.debug("Console requested")
             elif sPage =="/api/params":
                 # send the contents of the config file
-                resp=self.server.MC_Process.proc.stdout.read(1000)
+                # perhaps do this as a form which submits
+                fConfig=io.open(INI_FILE)
+                resp=fConfig.readall()
                 self.request.sendall(resp)
+                self.logger.debug("config requested")
             else:
                 print "|"+sPage+"|"
                 raise TypeError
@@ -153,7 +221,20 @@ class MinecraftTCPHandler(SocketServer.BaseRequestHandler):
         finally:            
             # clean up
             print "Clean Up"
-            
+
+def _getMC_PropsFiles():# construct the list of properties files available as an option list
+    return subprocess.check_call(GET_PROPS_CMD)
+
+def _loadMC_PropsFile(file_name)
+    # back up the existing file
+    # copy the new file over server.properties
+    return 0
+
+def _saveMC_PropsFile(file_name)
+    # save server.properties over new file name
+    file_Path=SERVER_ROOT+file_name
+    return 0
+    
 class MC_Thread(threading.Thread):# thread type
     """
     This is the thread that will run the Minecraft proccess
@@ -172,7 +253,7 @@ class MC_Thread(threading.Thread):# thread type
 	mem_tot=subprocess.check_call(FREE_MEM_CMD)
         # modify params for memory
 	if mem_tot>2000:
-		mem_param='1G'
+		mem_param='2G'
         elif mem_tot>1000:
                 mem_param='1G'
 	else:
@@ -180,12 +261,15 @@ class MC_Thread(threading.Thread):# thread type
 	mc_version=_getIniValue('server_version')
 	mc_path='minecraft_server.'+mc_version+'.jar'
         return ['sudo','java','-Xms'+mem_param,'-Xmx'+mem_param,'-jar',mc_path,'nogui']
+
     def run(self):
 	try:
+                self.logger = logging.getLogger(LOGGER_STREAM)
+                self.logger.debug("thread starting")
 		self.cmd=self.MC_CmdLine()
-        	os.chdir('/srv/minecraft')
+        	os.chdir(SERVER_ROOT)
 	        self.proc=subprocess.Popen(self.cmd,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
-#        self.proc=subprocess.Popen(self.cmd,stdin=subprocess.PIPE)
+                self.logger.debug("Process Started")
 	        self.MC_Input=self.proc.stdin
         	self.MC_Output=self.proc.stdout
 		self.MC_Log=io.open(LOG_FILE,'a')
@@ -193,33 +277,74 @@ class MC_Thread(threading.Thread):# thread type
 			outLine=self.MC_Output.readline()
 			self.MC_Log.writeline(outLine)
 	finally:
+                self.logger.debug("thread stopping")
 		self.MC_Log.close()
         
-class MC_Server:
-    """
+class MC_Server(SocketServer.TCPServer):
+"""
     This is the base class for the instance's data
-    it includes properties for the TCP server for the RPC calls
-    and stream handlers for the I/O of the minecraft process
-    both of these elements need to be contained in a separate thread
-    
+    it needs to be multithreaded.    
     """
-    def __init__(self):
-	PORT=_getIntParam('port',sys.argv,8000)
-	HOST =_getStrParam('host',sys.argv,"*")
+    def __init__(self,request,client_address,server):
+        self.logger = logging.getLogger(LOGGER_STREAM)
+        self.logger.debug('__init__')
+        SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
         self.MC_Process=MC_Thread()
-        self.RPC_Server=SocketServer.TCPServer((HOST, PORT), MinecraftTCPHandler())
-        self.RPC_Server.MC_Process=self.MC_Process
+        self.restarting=0
 
     def keep_running(self):
-        return self.MC_Process.is_alive()
-#	return 1
+        return self.MC_Process.is_alive() or self.restarting==1
 
     def serve_forever(self):
         self.MC_Process.start()
         while self.keep_running() :
-            self.RPC_Server.handle_request()
-#	    time.sleep(10)
-            
+            self.handle_request()
+
+    def restart_server(self):
+        self.logger.debug('Restarting')
+        self.restarting=1
+        Kill_Minecraft()
+        while self.MC_Process.is_alive()
+            time.sleep(1)
+        self.MC_Process=MC_Thread()
+        self.MC_Process.start()
+        self.restarting=0        
+        self.logger.debug('Restarted')
+
+def Detect_Minecraft():
+    return subprocess.check_call(GET_STATUS_CMD)
+
+def Kill_Minecraft():
+# Connect to the server instance and issue the /stop command
+    self.logger = logging.getLogger(LOGGER_STREAM)
+    self.logger.debug('Creating Kill socket')
+    stop_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    stop_sock.connect(('localhost',PORT))
+    try:
+        self.logger.debug('sending kill signal to localhost')
+	stop_sock.sendall(KILL_SIGNAL)
+	amt_rcv=0
+	amt_exp=2
+	while amt_rcv<amt_exp:
+            resp=stop_sock.recv(2)
+            amt_rcv=len(resp)
+            if resp==KILL_RESP_OK:
+                self.logger.debug('Kill Signal processed')
+		print "Process Killed"
+		reslt=0
+	    elif resp==KILL_RESP_BAD:
+                self.logger.debug('Kill Signal Failure')
+		print "Unable to kill process"
+		reslt=1
+	    else:
+		print "Bad Response Received"
+                self.logger.debug('Kill Signal Error')
+		reslt=1
+    finally:
+        self.logger.debug('Closing socket')
+	stop_sock.close()
+    return reslt
+    
 if __name__ == "__main__":    
     PORT=_getIntParam('port',sys.argv,8000)
     HOST =_getStrParam('host',sys.argv,"*")
@@ -228,37 +353,27 @@ if __name__ == "__main__":
 Usage
     minecraft_server.py --help
         display this message
-    minecraft_server.py [ port=<port> ] [ host=<host> ]
+    minecraft_server.py start [ port=<port> ] [ host=<host> ]
         start the Minecraft service rununing
-            """
+    minecraft_server.py stop
+        Stop the minecraft service
+    minecraft_server.py status
+        return the status of the service
+        """
     elif 'start' in sys.argv:
-        mc_Server=MC_Server()
+#        mc_Server=MC_Server()
+        mc_Server=MC_Server((HOST,PORT),MinecraftTCPHandler)
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
         mc_Server.serve_forever()
     elif 'stop' in sys.argv:
-#        print "Please connect to the server instance and issue the /stop command"
-        # or construct an HTTP request to localhost to stop the service
-	stop_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	stop_sock.connect(('localhost',PORT))
-	try:
-		stop_sock.sendall(KILL_SIGNAL)
-		amt_rcv=0
-		amt_exp=2
-		while amt_rcv<amt_exp:
-			resp=stop_sock.recv(2)
-			amt_rcv=len(resp)
-		if resp==KILL_RESP_OK:
-			print "Process Killed"
-			reslt=0
-		elif resp==KILL_RESP_BAD:
-			print "Unable to kill process"
-			reslt=1
-		else:
-			print "Bad Response Received"
-			reslt=1
-	finally:
-		stop_sock.close()
+        reslt=Kill_Minecraft() #invoke the process to kill the server
 	exit(reslt)
+    elif 'status' in sys.argv:
+	is_alive=Detect_Minecraft()
+        if is_alive==0 :
+            print "Minecraft Server is Live"
+        exit(is_alive)
     else:
         print "Unknown Command"
+        exit(1)
